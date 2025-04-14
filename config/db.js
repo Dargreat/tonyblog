1746042830cc1695fa7e3785d16ace93cab4adde
Dargreat <132403953+Dargreat@users.noreply.github.com>
2025-04-14 19:22:08 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        // Updated connection without deprecated options
+        await mongoose.connect(process.env.MONGO_URI);
+        
         console.log('MongoDB Connected');
     } catch (err) {
-        console.error(err);
+        console.error('Connection error:', err);
         process.exit(1);
     }
 };
